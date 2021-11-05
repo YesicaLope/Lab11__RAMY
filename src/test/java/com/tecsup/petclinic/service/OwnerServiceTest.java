08be7d571eda5bb1af5bdfa5ec70fb0106d82bfa
Mauricio-DelVillar-Canchari <User@192.168.1.9>
2021-11-05 14:08:31 -0500

    Cambios hechos

--- src/test/java/com/tecsup/petclinic/service/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/service/OwnerServiceTest.java
@@ -1,39 +1,43 @@
 package com.tecsup.petclinic.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tecsup.petclinic.domain.Owner;
+import com.tecsup.petclinic.exception.OwnertNotFoundException;
+import com.tecsup.petclinic.exception.PetNotFoundException;
 
 @SpringBootTest
 public class OwnerServiceTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(OwnerServiceTest.class);
 
-	//@Autowired
-   //	private OwnerService ownerService;
+	@Autowired
+	private OwnerService ownerService;
 
 	/**
+	 * @throws PetNotFoundException 
 	 * 
 	 */
-	/*
 	@Test
-	public void testFindOwnerById() {
+	public void testFindOwnerById() throws OwnertNotFoundException, PetNotFoundException {
 
 		long ID = 1;
 		String NAME = "Jaime";
 		Owner owner = null;
 		
-		try {
-			
-			owner = ownerService.findById(ID);
-			
-		} catch (OwnertNotFoundException e) {
-			fail(e.getMessage());
-		}
+		owner = ownerService.findById(ID);
 		logger.info("" + owner);
 
-		assertEquals(NAME, owner.getName());
+		assertEquals(NAME, owner.getFirstname());
 
 	}
-	*/
+	
 }
+
